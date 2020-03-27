8773b21834339a56ea83652dfc2391115c6c0631
a8hok <ashokkumar.explore@gmail.com>
2020-03-28 00:11:23 +0530

    todo edit flow

--- client/src/components/ListTodo.js
+++ client/src/components/ListTodo.js
@@ -1,24 +1,37 @@
 import React, { Fragment, useEffect, useState } from 'react';
+import EditTodo from './EditTodo';
 
 const ListTodo = () => {
   const [todos, setTodo] = useState([]);
+  // get all todos
   const getTodo = async () => {
     try {
       const response = await fetch('http://localhost:5000/todos');
       const jsonDate = await response.json();
       setTodo(jsonDate);
-      console.log(jsonDate)
     } catch (error) {
       console.error(error.message);
     }
   };
+  // delete todo
+  const deleteTodo = async (id) => {
+    try {
+      const todo = await fetch(`http://localhost:5000/todos/${id}`, {
+        method: 'DELETE',
+      });
+      setTodo(todos.filter((todo) => todo.todo_id !== id));
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+  // Trigger on window load
   useEffect(() => {
     getTodo();
   }, []);
   return (
     <Fragment>
       <h2 className='mt-5 center-text'>List Todo</h2>
-      <table class='table'>
+      <table className='table'>
         <thead>
           <tr>
             <th>Description</th>
@@ -28,10 +41,18 @@ const ListTodo = () => {
         </thead>
         <tbody>
           {todos.map((todo) => (
-            <tr>
+            <tr key={todo.todo_id}>
               <td>{todo.description}</td>
-              <td>Edit</td>
-              <td>Delete</td>
+              <td>
+                <EditTodo todo={todo} />
+              </td>
+              <td>
+                <button
+                  className='btn btn-danger'
+                  onClick={() => deleteTodo(todo.todo_id)}>
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
